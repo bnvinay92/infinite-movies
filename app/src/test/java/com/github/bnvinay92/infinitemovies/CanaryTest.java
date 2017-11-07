3442ee156a56402651e1e9036b5926d33d9535f1
Vinay Nagaraj <bnvinay92@gmail.com>
2017-11-07 20:56:04 +0530

    Setup project and add mutation coverage
    Add checkstyle & lint to buid process.

---
+++ app/src/test/java/com/github/bnvinay92/infinitemovies/CanaryTest.java
@@ -0,0 +1,13 @@
+package com.github.bnvinay92.infinitemovies;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CanaryTest {
+
+  @Test
+  public void canaryTest() throws Exception {
+    assertEquals(4, 2 + 2);
+  }
+}
